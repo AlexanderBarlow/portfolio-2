820879f122c80cc351b2e39173dc1a622e2d1b1c
alex barlow <alex.barlow@rocketmail.com>
2022-12-19 13:44:29 -0500

    added js

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,15 @@
 const themeButton = document.getElementById('flexSwitchCheckDefault');
+const darkTheme = document.getElementsByClass('dark-theme')
 
 //by default we have a light theme
 //apply dark theme on switch
 //add event listener for click on switch
 //add logic for what to do when button is clicked
+
+const themeToggle = () => {
+    //we need to add class dark theme upon click
+    //change content of button text to click for light theme
+
+}
+
+addEventListener('click', themeToggle)
