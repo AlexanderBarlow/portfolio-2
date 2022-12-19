350f34a4dd299a4ba912a405ea64513623d39ca1
alex barlow <alex.barlow@rocketmail.com>
2022-12-19 13:36:39 -0500

    added notes to js

---
+++ public/js/script.js
@@ -0,0 +1,6 @@
+const themeButton = document.getElementById('flexSwitchCheckDefault');
+
+//by default we have a light theme
+//apply dark theme on switch
+//add event listener for click on switch
+//add logic for what to do when button is clicked
